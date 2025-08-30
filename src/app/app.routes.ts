cbb9db3742564cfafffc5e2243fe3845c352c232
Kal <amjsimpson2013@gmail.com>
2025-08-30 00:28:40 -0400

    3-created header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,17 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     {
         path: 'home',
-        loadComponent: () => import('./home/home')
+        loadComponent: () => import('./home/home'),
+        title: 'Home'
     },
     {
         path: 'events',
-        loadComponent: () => import('./events/events')
+        loadComponent: () => import('./events/events'),
+        title: 'Events'
+    },
+    {
+        path: ' ',
+        pathMatch: 'full',
+        redirectTo: 'home'
     }
 ];
