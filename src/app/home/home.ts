f0d7e61e97953ec01dfde0c39161eeb82dd40103
amjsimpson2013 <75797990+amjsimpson2013@users.noreply.github.com>
2025-10-29 21:11:18 -0400

    Add Facebook Post Component (#17)

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,21 +1,24 @@
 import { Component, inject, Signal } from '@angular/core';
 import { AdvertComponent } from "./ui/advert-component/advert-component";
 import { MatDividerModule } from '@angular/material/divider';
-import { GCalAdvert } from '../shared/models/gcal-state';
-import { GcalService } from '../shared/data-access/gcal-service';
 import { AdvertisementService } from './data-access/advertisement-service';
 import { Advertisement } from './data-access/models/advertisement-model';
 import {MatCardModule} from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { BlogPostComponent } from "./ui/blog-post-component/blog-post-component";
+import { PostService } from './data-access/post-service';
+import { Post } from './data-access/models/post-model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrl: './home.scss',
-  imports: [AdvertComponent, MatDividerModule, MatCardModule, MatIconModule]
+  imports: [AdvertComponent, MatDividerModule, MatCardModule, MatIconModule, BlogPostComponent]
 })
 export default class Home {
   advertisementService: AdvertisementService = inject(AdvertisementService);
+  postService: PostService = inject(PostService);
 
   ads: Signal<Advertisement[] | undefined> = this.advertisementService.ads;
+  posts: Signal<Post[] | undefined> = this.postService.posts;
 }
