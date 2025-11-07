8dfdc7d4b5e3a9f6d0c55b0c09a869bc531e46ef
amjsimpson2013 <75797990+amjsimpson2013@users.noreply.github.com>
2025-11-06 20:34:19 -0500

    Added Team Member Cards (#45)

---
+++ src/app/about/data-access/models/team-member.model.ts
@@ -0,0 +1,70 @@
+export interface TeamMember {
+    name: string;
+    imageUrl: string;
+    description: string;
+}
+
+export type TeamMembers = TeamMember[];
+
+export const TeamMembers: TeamMembers = [
+    {
+        name: 'Team Member',
+        imageUrl: '/assets/logo.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
+        'Mauris tempor pretium justo et tincidunt. Vivamus tortor massa, accumsan ' +
+        'aliquet tortor condimentum, efficitur sollicitudin nulla. Aliquam in euismod ' +
+        'nunc. Nulla facilisi. Duis nec dui nulla. Sed accumsan lectus ac lorem accumsan, ' + 
+        'vel pretium magna condimentum. Pellentesque sit amet vestibulum libero. Maecenas ' + 
+        'eu felis nisl. Maecenas accumsan est vel ullamcorper maximus. Praesent non quam ' +
+        'eu ex euismod vehicula. Proin et nibh vestibulum, aliquet est non, bibendum nisl. ' +
+        'Vestibulum interdum facilisis turpis nec egestas.'
+    },
+    {
+        name: 'Team Member',
+        imageUrl: '/assets/logo.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
+        'Mauris tempor pretium justo et tincidunt. Vivamus tortor massa, accumsan ' +
+        'aliquet tortor condimentum, efficitur sollicitudin nulla. Aliquam in euismod ' +
+        'nunc. Nulla facilisi. Duis nec dui nulla. Sed accumsan lectus ac lorem accumsan, ' + 
+        'vel pretium magna condimentum. Pellentesque sit amet vestibulum libero. Maecenas ' + 
+        'eu felis nisl. Maecenas accumsan est vel ullamcorper maximus. Praesent non quam ' +
+        'eu ex euismod vehicula. Proin et nibh vestibulum, aliquet est non, bibendum nisl. ' +
+        'Vestibulum interdum facilisis turpis nec egestas.'
+    },
+    {
+        name: 'Team Member',
+        imageUrl: '/assets/logo.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
+        'Mauris tempor pretium justo et tincidunt. Vivamus tortor massa, accumsan ' +
+        'aliquet tortor condimentum, efficitur sollicitudin nulla. Aliquam in euismod ' +
+        'nunc. Nulla facilisi. Duis nec dui nulla. Sed accumsan lectus ac lorem accumsan, ' + 
+        'vel pretium magna condimentum. Pellentesque sit amet vestibulum libero. Maecenas ' + 
+        'eu felis nisl. Maecenas accumsan est vel ullamcorper maximus. Praesent non quam ' +
+        'eu ex euismod vehicula. Proin et nibh vestibulum, aliquet est non, bibendum nisl. ' +
+        'Vestibulum interdum facilisis turpis nec egestas.'
+    },
+    {
+        name: 'Team Member',
+        imageUrl: '/assets/logo.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
+        'Mauris tempor pretium justo et tincidunt. Vivamus tortor massa, accumsan ' +
+        'aliquet tortor condimentum, efficitur sollicitudin nulla. Aliquam in euismod ' +
+        'nunc. Nulla facilisi. Duis nec dui nulla. Sed accumsan lectus ac lorem accumsan, ' + 
+        'vel pretium magna condimentum. Pellentesque sit amet vestibulum libero. Maecenas ' + 
+        'eu felis nisl. Maecenas accumsan est vel ullamcorper maximus. Praesent non quam ' +
+        'eu ex euismod vehicula. Proin et nibh vestibulum, aliquet est non, bibendum nisl. ' +
+        'Vestibulum interdum facilisis turpis nec egestas.'
+    },
+    {
+        name: 'Team Member',
+        imageUrl: '/assets/logo.png',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' + 
+        'Mauris tempor pretium justo et tincidunt. Vivamus tortor massa, accumsan ' +
+        'aliquet tortor condimentum, efficitur sollicitudin nulla. Aliquam in euismod ' +
+        'nunc. Nulla facilisi. Duis nec dui nulla. Sed accumsan lectus ac lorem accumsan, ' + 
+        'vel pretium magna condimentum. Pellentesque sit amet vestibulum libero. Maecenas ' + 
+        'eu felis nisl. Maecenas accumsan est vel ullamcorper maximus. Praesent non quam ' +
+        'eu ex euismod vehicula. Proin et nibh vestibulum, aliquet est non, bibendum nisl. ' +
+        'Vestibulum interdum facilisis turpis nec egestas.'
+    },
+]
